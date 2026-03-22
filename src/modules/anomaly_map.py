818f9ba9d14f3b916bd7fbc42d00abee215cdf0a
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-03-22 22:11:30 +0300

    Update anomaly_map.py

--- src/modules/anomaly_map.py
+++ src/modules/anomaly_map.py
@@ -1,5 +1,9 @@
 import torch
 
-
-def compute_anomaly_map(f, f_hat):
-    return torch.sum((f - f_hat) ** 2, dim=1, keepdim=True)
+def compute_anomaly_map(f, f_hat, use_residual=True):
+    if use_residual:
+        r = f - f_hat  
+    else:
+        r = f_hat  
+    anomaly_map = torch.sum(r ** 2, dim=1, keepdim=True)
+    return anomaly_map
